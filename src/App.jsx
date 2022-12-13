552ee977c7f2757fd85d9ae881fb5563ae085c64
Bunyameen <bunyameen@gmail.com>
2022-12-13 18:57:58 +0100

    Random Quote: added a function to generate quote randomly and generate color

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,30 @@ import {Quotes} from "./Quote"
 
 function App() {
   const [quote, setQuote] = useState([])
-  // 
+  const [color, setColor] = useState('')
+  
 
 
   const getRandomQuote = () =>{
     let randomQuote = Math.floor(Math.random() * Quotes.length)
-    setQuote(Quotes[randomQuote])
+    setQuote(Quotes[randomQuote]);
+
+    let str ="0123456789abcdef";
+    let hexCode = ""
+
+    for(let i = 0; i < 6; i++){
+      const colorIdx = Math.floor(Math.random() * str.length)
+      hexCode += str[colorIdx]
+      setColor(`#${hexCode}`)
+
+
+
+    }
     
   }
 
   console.log(quote);
+  console.log(color)
 
  useEffect(() => {
 
