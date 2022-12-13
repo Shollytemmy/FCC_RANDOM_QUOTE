ee15db0a56c986b03a6478fd48fae76b27066d0a
Bunyameen <bunyameen@gmail.com>
2022-12-13 11:14:11 +0100

    Quote Machine: added Array of quotes & Athurs

---
+++ src/Quote.js
@@ -0,0 +1,27 @@
+const Quotes = [
+    {
+        id: 1,
+        authur: "Michael Jordan",
+        content: "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed."
+    },
+    {
+        id: 2,
+        authur: "Henry David Thoreau",
+        content: "Many men go fishing all of their lives without knowing that it is not fish they are after."
+    },
+    {
+        id: 3,
+        authur: "Vince Lombardi",
+        content: "Winners never quit and quitters never win."
+    },
+    {
+        id: 4,
+        authur: "Babe Ruth",
+        content: "The way a team plays as a whole determines its success. You may have the greatest bunch of individual stars in the world, but if they don't play together, the club won't be worth a dime."
+    },
+    {
+        id: 5,
+        authur: "Bob Feller",
+        content: "Every day is a new opportunity. You can build on yesterday's success or put its failures behind and start over again. That's the way life is, with a new game every day, and that's the way baseball is."
+    }
+]
